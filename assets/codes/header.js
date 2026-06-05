644b1d11fec336c6bf232a2f690f18e9c1f44328
sharelation <theta0913@outlook.jp>
2026-06-05 20:22:03 +0900

    changed header

---
+++ assets/codes/header.js
@@ -0,0 +1,23 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    headercolor();
+});
+
+setInterval(headercolor, 1000);
+
+function headercolor() {
+    let y = window.scrollY;
+    const nav = header.querySelector("nav");
+    if (y >= 0 && y <= 300) {
+        header.style.background = `rgba(248, 247, 245, ${y / 300})`;
+        for (let i = 0; i < nav.children.length; i++) {
+            nav.children[i].style.color = `hsl(0, 0%, ${100 - (y / 3)}%)`;
+        }
+    } else if (y > 300) {
+        header.style.background = "rgba(248, 247, 245, 1)";
+        for (let i = 0; i < nav.children.length; i++) {
+            nav.children[i].style.color = "hsl(0, 0%, 0%)";
+        }
+    }
+}
