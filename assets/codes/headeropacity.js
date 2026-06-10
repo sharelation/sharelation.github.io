9db1270c59ca604d2113e840ef7a8d0e837df52f
sharelation <theta0913@outlook.jp>
2026-06-10 23:08:32 +0900

    add introduce making

---
+++ assets/codes/headeropacity.js
@@ -0,0 +1,21 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    headercolor();
+});
+
+function headercolor() {
+    let y = window.scrollY;
+    const nav = header.querySelector("nav");
+    if (y >= 0 && y <= 300) {
+        header.style.background = `rgba(248, 247, 245, ${y / 300})`;
+        for (let i = 0; i < document.getElementById("navancher").children.length; i++) {
+            document.getElementById("navancher").children[i].style.color = `hsl(0, 0%, ${100 - (y / 3)}%)`;
+        }
+    } else if (y > 300) {
+        header.style.background = "rgba(248, 247, 245, 1)";
+        for (let i = 0; i < document.getElementById("navancher").children.length; i++) {
+            document.getElementById("navancher").children[i].style.color = "hsl(0, 0%, 0%)";
+        }
+    }
+}
--- assets/codes/headeropen.js
+++ assets/codes/headeropen.js
@@ -1,28 +1,7 @@
+
 const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-    headercolor();
-});
-
-setInterval(headercolor, 1000);
-
-function headercolor() {
-    let y = window.scrollY;
-    const nav = header.querySelector("nav");
-    if (y >= 0 && y <= 300) {
-        header.style.background = `rgba(248, 247, 245, ${y / 300})`;
-        for (let i = 0; i < document.getElementById("navancher").children.length; i++) {
-            document.getElementById("navancher").children[i].style.color = `hsl(0, 0%, ${100 - (y / 3)}%)`;
-        }
-    } else if (y > 300) {
-        header.style.background = "rgba(248, 247, 245, 1)";
-        for (let i = 0; i < document.getElementById("navancher").children.length; i++) {
-            document.getElementById("navancher").children[i].style.color = "hsl(0, 0%, 0%)";
-        }
-    }
-}
-
 let navopen = false;
+
 addEventListener("click", (e) => {
     if (e.target.id == "hamburger" || e.target.parentElement.id == "hamburger") {
         const navancher = document.getElementById("navancher");
